472856eb681d58d025e71c0e3cc5719e7f393a12
wu529778790 <529778790@qq.com>
2024-05-19 22:37:48 +0800

    fix: 修改错误日志

--- src/server/deploy/index.js
+++ src/server/deploy/index.js
@@ -6,14 +6,14 @@ const router = express.Router();
 router.get("/", (req, res) => {
   exec("bash deploy.sh", (error, stdout, stderr) => {
     if (error) {
-      console.error(`执行 ${deployScript} 脚本时发生错误：${error.message}`);
+      console.error(`执行脚本时发生错误：${error.message}`);
       return;
     }
     if (stderr) {
-      console.error(`执行 ${deployScript} 脚本时出现错误：${stderr}`);
+      console.error(`执行脚本时出现错误：${stderr}`);
       return;
     }
-    console.log(`执行 ${deployScript} 脚本成功，输出：${stdout}`);
+    console.log(`执行脚本成功，输出：${stdout}`);
     res.send("success");
   });
 });
