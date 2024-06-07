276d705b98782ef848c29ea98ffab8cdd5808a81
wuwei <wuwei@fadada.com>
2024-06-07 14:51:43 +0800

    fix: 虚拟列表增加上下缓冲

--- src/client/views/index/components/VitualList/index.vue
+++ src/client/views/index/components/VitualList/index.vue
@@ -5,7 +5,11 @@
       :style="{ height: totalHeight + 'px' }"></div>
     <div
       class="virtual-list"
-      :style="{ transform: `translateY(${startTop}px)` }">
+      :style="{
+        top: -(topBufferLength * itemHeight) + 'px',
+        bottom: -(bottomBufferLength * itemHeight) + 'px',
+        transform: `translateY(${startOffset}px)`,
+      }">
       <div
         v-for="(item, index) in virtualList"
         :key="item"
@@ -37,27 +41,46 @@ const props = defineProps({
 });
 
 const totalHeight = computed(() => props.data.length * props.itemHeight);
-const start = ref(0);
+// 起始索引
+const startIndex = ref(0);
+// 显示行数
 const count = ref(0);
-const end = computed(() => start.value + count.value);
+// 结束索引
+const endIndex = ref(0);
+// 缓冲起始索引
+const bufferStartIndex = computed(() => {
+  return Math.max(0, startIndex.value - props.buffer);
+});
+// 缓冲结束索引
+const bufferEndIndex = computed(() => {
+  return Math.min(endIndex.value + props.buffer, props.data.length);
+});
+// 顶部缓冲行数
+const topBufferLength = computed(() => {
+  return Math.max(0, startIndex.value - bufferStartIndex.value);
+});
+// 底部缓冲行数
+const bottomBufferLength = computed(() => {
+  return Math.max(0, bufferEndIndex.value - endIndex.value);
+});
+
 const virtualList = computed(() => {
-  return props.data.slice(start.value, end.value);
+  return props.data.slice(bufferStartIndex.value, bufferEndIndex.value);
 });
 
 onBeforeMount(() => {
   const { innerHeight } = window;
-  count.value = Math.ceil(innerHeight / props.itemHeight) + props.buffer;
+  count.value = Math.ceil(innerHeight / props.itemHeight);
+  endIndex.value = startIndex.value + count.value;
 });
 
-const startTop = ref(0);
+const startOffset = ref(0);
 const emit = defineEmits(["scrollToBottom"]);
 const onScroll = (e) => {
   const { scrollTop, clientHeight, scrollHeight } = e.target;
-  start.value = Math.floor(scrollTop / props.itemHeight);
-  startTop.value =
-    scrollTop % props.itemHeight
-      ? Math.floor(scrollTop / props.itemHeight) * props.itemHeight
-      : scrollTop;
+  startIndex.value = Math.floor(scrollTop / props.itemHeight);
+  endIndex.value = Math.min(startIndex.value + count.value, props.data.length);
+  startOffset.value = scrollTop - (scrollTop % props.itemHeight);
   // 如果滚动到底部
   if (scrollTop + clientHeight === scrollHeight) {
     emit("scrollToBottom");
@@ -75,11 +98,11 @@ const onScroll = (e) => {
     left: 0;
     top: 0;
     right: 0;
+    bottom: 0;
   }
   .virtual-list {
     position: absolute;
     left: 0;
-    top: 0;
     right: 0;
   }
 }
