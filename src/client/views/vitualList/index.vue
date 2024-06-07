83246012ab55f6c5b4787ee5b5b907effc3938af
wuwei <wuwei@fadada.com>
2024-06-07 14:28:57 +0800

    feat: 用dom方式增加上下缓冲

--- src/client/views/vitualList/index.vue
+++ src/client/views/vitualList/index.vue
@@ -5,12 +5,36 @@
       :style="{ height: totalHeight + 'px' }"></div>
     <div
       class="virtual-list"
-      :style="{ transform: `translate3d(0, ${startOffset}px, 0)` }">
+      :style="{
+        top: -(topBufferList.length * itemHeight) + 'px',
+        bottom: -(bottomBufferList.length * itemHeight) + 'px',
+        transform: `translate3d(0, ${startOffset}px, 0)`,
+      }">
+      <!-- 顶部缓冲 -->
       <div
-        v-for="(item, index) in virtualList"
+        class="item"
+        v-for="item in topBufferList"
+        :key="item"
+        :style="{
+          height: itemHeight + 'px',
+          background: '#f0fcff',
+        }">
+        {{ item }}
+      </div>
+      <!-- 中间内容 -->
+      <div
+        v-for="item in virtualList"
         :key="item"
         class="item"
         :style="{ height: itemHeight + 'px' }">
+        {{ item }}
+      </div>
+      <!-- 底部缓冲 -->
+      <div
+        class="item"
+        v-for="item in bottomBufferList"
+        :key="item"
+        :style="{ height: itemHeight + 'px', background: '#d6ecf0' }">
         {{ item }}
       </div>
     </div>
@@ -30,6 +54,22 @@ const startIndex = ref(0);
 const count = ref(0);
 // 结束索引
 const endIndex = ref(0);
+// 缓冲行数
+const buffer = ref(2);
+// 顶部缓冲
+const topBufferList = computed(() => {
+  return data.value.slice(
+    Math.max(0, startIndex.value - buffer.value),
+    startIndex.value
+  );
+});
+// 底部缓冲
+const bottomBufferList = computed(() => {
+  return data.value.slice(
+    endIndex.value,
+    Math.min(endIndex.value + buffer.value, data.value.length)
+  );
+});
 
 const virtualList = computed(() => {
   return data.value.slice(startIndex.value, endIndex.value);
@@ -60,11 +100,11 @@ const onScroll = (event) => {
     left: 0;
     top: 0;
     right: 0;
+    bottom: 0;
   }
   .virtual-list {
     position: absolute;
     left: 0;
-    top: 0;
     right: 0;
     .item {
       background-color: #eee;
